4caf957776db07846481cd36e91b729a82e5f7d4
sagardesai <desaisagar8090@gmail.com>
2026-04-28 18:52:27 +0530

    Add getter

--- src/main/java/com/sagar/dto/ResponseDTO.java
+++ src/main/java/com/sagar/dto/ResponseDTO.java
@@ -2,12 +2,14 @@ package com.sagar.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ResponseDTO <T> {
+public class ResponseDTO<T> {
     private String msg;
     private int statusCode;
     private T data;
